63767b269e0ef37e60e3face8b770faa8454da0f
HLupo <hugo.lupo@epitech.eu>
2023-06-14 19:03:16 +0200

    todo page, todo components and custom hooks. | Todo prisma schema and CRUD todo api router

--- src/server/api/routers/todo.ts
+++ src/server/api/routers/todo.ts
@@ -5,4 +5,39 @@ export const todoRouter = createTRPCRouter({
   getAll: privateProcedure.query(({ ctx }) => {
     return ctx.prisma.todo.findMany({ where: { createdBy: ctx.userId } });
   }),
+  create: privateProcedure
+    .input(
+      z.object({
+        title: z.string().min(1).max(255),
+        description: z.string().min(1).max(255),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const creatorId = ctx.userId;
+
+      const todo = await ctx.prisma.todo.create({
+        data: {
+          title: input.title,
+          description: input.description,
+          createdBy: creatorId,
+        },
+      });
+      return todo;
+    }),
+  updateDone: privateProcedure
+    .input(z.object({ done: z.boolean(), id: z.string().cuid() }))
+    .mutation(async ({ ctx, input }) => {
+      const todo = await ctx.prisma.todo.update({
+        where: { id: input.id },
+        data: { done: input.done },
+      });
+      return todo;
+    }),
+  delete: privateProcedure
+    .input(z.object({ id: z.string().cuid() }))
+    .mutation(async ({ ctx, input }) => {
+      await ctx.prisma.todo.delete({
+        where: { id: input.id },
+      });
+    }),
 });
